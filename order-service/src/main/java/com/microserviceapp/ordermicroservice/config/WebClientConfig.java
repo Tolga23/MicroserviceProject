73dbf954b1f7a028a619180202e1cf3d12b4fb30
Tolga <10477308+Tolga23@users.noreply.github.com>
2023-08-09 15:44:33 +0300

    discovery service eureka client added

--- order-service/src/main/java/com/microserviceapp/ordermicroservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/microserviceapp/ordermicroservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.microserviceapp.ordermicroservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    @LoadBalanced // This annotation is used to call the service using service name instead of IP address
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
     }
-}
+}   // WebClient.Builder is used to build the web client object
+    // @LoadBalanced annotation is used to call the service using service name instead of IP address
